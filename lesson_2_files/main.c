ebee06b4d40ff307c8b723a935ad526054e5ab33
yantanyi <webside@tooeh.xyz>
2022-10-05 20:31:10 +0800

    int_sizeof_long

--- lesson_2_files/main.c
+++ lesson_2_files/main.c
@@ -2,7 +2,7 @@
  * @Author: yantanyi webside@tooeh.xyz
  * @Date: 2022-10-03 21:12:12
  * @LastEditors: yantanyi webside@tooeh.xyz
- * @LastEditTime: 2022-10-05 14:11:39
+ * @LastEditTime: 2022-10-05 20:27:57
  * @FilePath: /dir_for_C_learning/lesson_2_files/main.c
  * @Description:
  *
@@ -16,6 +16,7 @@ int main(void)
     //  666 eee jjj    print_test(3);
     set_test_number(18);
     call_print_test(get_test_number());
+    printf("%d\n", (int)sizeof(int));
     printf("Hello_differs_9\n");
     return 0;
 }
