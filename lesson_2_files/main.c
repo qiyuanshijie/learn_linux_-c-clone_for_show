44fd9423ff396a7e84a267428655f1a55fb813b3
yantanyi <webside@tooeh.xyz>
2022-10-05 13:18:18 +0800

    这里可随意输入内容，也可随意换行，是提交的相关信息备注。

--- lesson_2_files/main.c
+++ lesson_2_files/main.c
@@ -2,7 +2,7 @@
  * @Author: yantanyi webside@tooeh.xyz
  * @Date: 2022-10-03 21:12:12
  * @LastEditors: yantanyi webside@tooeh.xyz
- * @LastEditTime: 2022-10-05 12:46:32
+ * @LastEditTime: 2022-10-05 13:05:15
  * @FilePath: /dir_for_C_learning/lesson_2_files/main.c
  * @Description:
  *
@@ -13,7 +13,7 @@
 #include "test.h"
 int main(void)
 {
-    //  666       print_test(3);
+    //  666 eee     print_test(3);
     set_test_number(18);
     call_print_test(get_test_number());
     printf("Hello_differs_1\n");
