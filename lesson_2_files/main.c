e984b3312dffa386e5ce6f516b8d1d707c86b1e4
yantanyi <webside@tooeh.xyz>
2022-10-05 10:28:23 +0800

    auto

--- lesson_2_files/main.c
+++ lesson_2_files/main.c
@@ -2,7 +2,7 @@
  * @Author: yantanyi webside@tooeh.xyz
  * @Date: 2022-10-03 21:12:12
  * @LastEditors: yantanyi webside@tooeh.xyz
- * @LastEditTime: 2022-10-05 10:16:53
+ * @LastEditTime: 2022-10-05 10:26:30
  * @FilePath: /dir_for_C_learning/lesson_2_files/main.c
  * @Description:
  *
@@ -14,7 +14,7 @@
 int main(void)
 {
     //    print_test(3);
-    set_test_number(6);
+    set_test_number(8);
     call_print_test(get_test_number());
     printf("Hello_differs_1\n");
     return 0;
