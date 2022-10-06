8102b24552281270255c594eed8c5e4bfa06f983
yantanyi <webside@tooeh.xyz>
2022-10-06 17:26:20 +0800

    x

--- lesson_2_files/main.c
+++ lesson_2_files/main.c
@@ -2,7 +2,7 @@
  * @Author: yantanyi webside@tooeh.xyz
  * @Date: 2022-10-03 21:12:12
  * @LastEditors: yantanyi webside@tooeh.xyz
- * @LastEditTime: 2022-10-06 17:09:29
+ * @LastEditTime: 2022-10-06 17:25:50
  * @FilePath: /dir_for_C_learning/lesson_2_files/main.c
  * @Description:
  *
@@ -13,6 +13,7 @@
 #include "test.h"
 int main(void)
 {
+
     //  666 eee jjj   22 print_test(3);
     set_test_number(100);
     call_print_test(get_test_number());
