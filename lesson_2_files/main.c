a16117cb1ee89efc540acf2b99fe56c79392f3c6
yantanyi <webside@tooeh.xyz>
2022-10-13 00:27:45 +0800

    qqq

--- lesson_2_files/main.c
+++ lesson_2_files/main.c
@@ -2,7 +2,7 @@
  * @Author: yantanyi webside@tooeh.xyz
  * @Date: 2022-10-03 21:12:12
  * @LastEditors: yantanyi webside@tooeh.xyz
- * @LastEditTime: 2022-10-05 20:27:57
+ * @LastEditTime: 2022-10-10 19:20:47
  * @FilePath: /dir_for_C_learning/lesson_2_files/main.c
  * @Description:
  *
@@ -14,6 +14,12 @@
 int main(void)
 {
     //  666 eee jjj    print_test(3);
+    int a[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    memcpy(a + 3, a, 5);
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%d ", a[i]);
+    }
     set_test_number(18);
     call_print_test(get_test_number());
     printf("%d\n", (int)sizeof(int));
