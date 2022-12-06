6d17b032d443df89ea89683edb6b557d4b05817d
yantanyi <webside@tooeh.xyz>
2022-12-06 19:15:27 +0800

    reflash

--- lesson_2_files/main.c
+++ lesson_2_files/main.c
@@ -2,7 +2,7 @@
  * @Author: yantanyi webside@tooeh.xyz
  * @Date: 2022-10-03 21:12:12
  * @LastEditors: yantanyi webside@tooeh.xyz
- * @LastEditTime: 2022-10-27 03:03:55
+ * @LastEditTime: 2022-10-28 10:21:40
  * @FilePath: /dir_for_C_learning/lesson_2_files/main.c
  * @Description:
  *
@@ -13,7 +13,6 @@
 #include "test.h"
 int main(void)
 {
-
     //  666 eee jjj   22 print_test(3);
     set_test_number(10);
     call_print_test(get_test_number());
